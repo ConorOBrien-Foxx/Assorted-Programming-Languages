f2ff93b706e8afd5bde95bfcb7d26b67b290f320
Conor O'Brien <conorobrien4god@gmail.com>
2016-04-08 10:36:56 -0400

    fix scoping issues

--- dotKill/dotKill.js
+++ dotKill/dotKill.js
@@ -1,14 +1,14 @@
 function modify(str){
-  c=function(e){return e.charCodeAt()}
-  str = str.split("\n").map(function(e){return e.split("").map(c);});
-  str2 = str.map(function(e){return e.slice().reverse()}).reverse();
+  var c=function(e){return e.charCodeAt()}
+  var str = str.split("\n").map(function(e){return e.split("").map(c);});
+  var str2 = str.map(function(e){return e.slice().reverse()}).reverse();
   return str.map(function(e,i){
     return e.map(function(f,j){
       return H(String.fromCharCode(Math.floor(i+j+(str2[i][j]+f)/2)));
     }).join("")
   }).join("\n");
 }
-function H(x){if(x.charCodeAt()<32)return f(String.fromCharCode(x.charCodeAt()+95));if(x.charCodeAt()>126)return f(String.fromCharCode(x.charCodeAt()-95));return x;}
+function H(x){if(x.charCodeAt()<32)return H(String.fromCharCode(x.charCodeAt()+95));if(x.charCodeAt()>126)return H(String.fromCharCode(x.charCodeAt()-95));return x;}
 function dotKill(e){
   code = modify(e);
   var res = "";
